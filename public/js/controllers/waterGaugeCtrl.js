3e20eb401333142c7e9ee6fc0ee4f131a023fa8e
leandro2m <lmmoraes@gmail.com>
2017-07-02 17:05:00 -0300

    better logic for piechart

--- public/js/controllers/waterGaugeCtrl.js
+++ public/js/controllers/waterGaugeCtrl.js
@@ -11,7 +11,6 @@ queue()
 	console.log("Tamanho do array = " + dataSet.length)
 
 	//
-
 	lastpost = new Date(dataSet[0].datetime);
 	console.log("Data do Ultimo Post " + lastpost)
 	dataAtual = new Date();
@@ -35,50 +34,6 @@ queue()
 		imgStatus.src = './images/offline.png';
 	}
 
-	var countCistern10 = 0; //Cisterna 10;
-	var countCistern10_L0 = 0 // Level 0 Cisterna 10
-	var countCistern10_L1 = 0
-	var countCistern10_L2 = 0
-	var countCistern10_L3 = 0
-	var countCistern10_L4 = 0
-
-
-	console.log("Pega o primeiro indice : " + dataSet[0].sensorid)
-
-	for (var i = 0; i < dataSet.length; i++) {
-
-		if (dataSet[i].sensorid == "UCSCistern10") {
-			countCistern10 = countCistern10 + 1;
-			if (dataSet[i].level4 == 1){
-				countCistern10_L4 = countCistern10_L4 + 1;
-			} else if (dataSet[i].level3 == 1) {
-				countCistern10_L3 = countCistern10_L3 + 1;
-			} 
-			else if (dataSet[i].level2 == 1) {
-				countCistern10_L2 = countCistern10_L2 + 1;
-			} else if (dataSet[i].level1 == 1) {
-				countCistern10_L1 = countCistern10_L1 + 1;
-			} else countCistern10_L0 = countCistern10_L0 + 1;
-		}
-
-	}
-	/*console.log("Numero de Vezes que Cisterna10 apareceu: " + countCistern10);
-	console.log("Numero de Vezes que Cisterna Level 0 apareceu: " + countCistern10_L0);
-	console.log("Numero de Vezes que Cisterna Level 1 apareceu: " + countCistern10_L1);
-	console.log("Numero de Vezes que Cisterna Level 2 apareceu: " + countCistern10_L2);
-	console.log("Numero de Vezes que Cisterna Level 3 apareceu: " + countCistern10_L3); */
-
-	calculaPercentL0 = Math.ceil((countCistern10_L0/countCistern10) * 100);
-	calculaPercentL1 = Math.ceil((countCistern10_L1/countCistern10) * 100);
-	calculaPercentL2 = Math.ceil((countCistern10_L2/countCistern10) * 100);
-	calculaPercentL3 = Math.ceil((countCistern10_L3/countCistern10) * 100);
-	calculaPercentL4 = Math.ceil((countCistern10_L4/countCistern10) * 100);
-
-	console.log("Percentual L0: " + calculaPercentL0);
-	console.log("Percentual L1: " + calculaPercentL1);
-	console.log("Percentual L2: " + calculaPercentL2);
-	console.log("Percentual L3: " + calculaPercentL3);
-	console.log("Percentual L4: " + calculaPercentL4);
 	
 		dataSet.forEach(function(d) {
 		d.datetime = dateFormat.parse(d.datetime);
@@ -114,8 +69,11 @@ queue()
 	var timeDim = ndx.dimension(function(d) {return new Date(d.datetime).getTime()});
 	var sensorDim = ndx.dimension(function(d) {return d.sensorid });
 	var blocoDim = ndx.dimension(function(d) {return d.blocoid });
-	
-	
+	var sensorLevel4 = ndx.dimension(function(d) {return d.level4 });
+	var sensorLevel3 = ndx.dimension(function(d) {return d.level3 });
+	var sensorLevel2 = ndx.dimension(function(d) {return d.level2 });
+	var sensorLevel1 = ndx.dimension(function(d) {return d.level1 });
+
 	
 	// ultima leitura Cisterna Bloco 10
 	sensorDim.filterAll();
@@ -129,32 +87,65 @@ queue()
 		var lastVolC10 = 0;
 		var colorC10c =  "#800080";
 	}
-	
-	// ultima leitura Caixa Bloco 10
-	sensorDim.filterAll();
-	sensorDim.filter(function(d) {return d === 'UCSReserv10'});
-	blocoDim.filter(function(d) {return d == 10});
-	if (timeDim.top(1)[0] != null) {
-		var lastVolR10 = timeDim.top(1)[0].total;
-		var colorC10r = timeDim.top(1)[0].changecolor;
-	}
-	else {
-		var lastVolR10 = 0;
-		var colorC10r =  "#800080";
-	}
+	//total de entradas no array
+	var totalEntradasC10 = ndx.groupAll().reduceCount().value();
+	console.log("Total de Leituras: " + totalEntradasC10)
+	sensorLevel1.filter(0);
+	sensorLevel2.filter(0);
+	sensorLevel3.filter(0);
+	sensorLevel4.filter(0);
+	var totalLevel0C10 = ndx.groupAll().reduceCount().value();
+	console.log("Quantidade de L0 C10 = 1: " +totalLevel0C10);
 
+	//Filtra Qtdade Sensor Level 1
 
-	//status bomba
-	/*sensorDim.filterAll();
-	sensorDim.filter(function(d) {return d === 'UCSCistern10'});
-	blocoDim.filter(function(d) {return d == 10});
-	if (timeDim.top(1)[0] != null) {
-		var bomba = timeDim.top(1)[0].bomba;
-	}
-	else {
-		var bomba = "Offline";
-		
-	}*/
+	sensorLevel1.filter(1);
+	sensorLevel2.filter(0);
+	sensorLevel3.filter(0);
+	sensorLevel4.filter(0);
+	var totalLevel1C10 = ndx.groupAll().reduceCount().value();
+	console.log("Quantidade de L1 C10 = 1: "+ totalLevel1C10);
+
+	//Filtra Qtade Sensor Level 2
+
+	sensorLevel1.filter(1);
+	sensorLevel2.filter(1);
+	sensorLevel3.filter(0);
+	sensorLevel4.filter(0);
+	var totalLevel2C10 = ndx.groupAll().reduceCount().value();
+	console.log("Quantidade de L2 C10 = 1: " +totalLevel2C10);
+
+	//Filtra Sensor Level3
+	sensorLevel1.filter(1);
+	sensorLevel2.filter(1);
+	sensorLevel3.filter(1);
+	sensorLevel4.filter(0);
+	var totalLevel3C10 = ndx.groupAll().reduceCount().value();
+	console.log("Quantidade de L3 C10 = 1: "+totalLevel3C10)
+
+	//Filtra Qtdade Sensor Level4
+
+	sensorLevel1.filter(1);
+	sensorLevel2.filter(1);
+	sensorLevel3.filter(1);
+	sensorLevel4.filter(1);
+	var totalLevel4C10 = ndx.groupAll().reduceCount().value();
+	console.log("Quantidade de L4 C10 = 1: "+totalLevel4C10);
+
+	//Filtra Qtdade Sensor Level0
+
+	calculaPercentL0 = Math.ceil((totalLevel0C10/totalEntradasC10) * 100);
+	calculaPercentL1 = Math.ceil((totalLevel1C10/totalEntradasC10) * 100);
+	calculaPercentL2 = Math.ceil((totalLevel2C10/totalEntradasC10) * 100);
+	calculaPercentL3 = Math.ceil((totalLevel3C10/totalEntradasC10) * 100);
+	calculaPercentL4 = Math.ceil((totalLevel4C10/totalEntradasC10) * 100);
+
+	console.log("Percentual L0: " + calculaPercentL0);
+	console.log("Percentual L1: " + calculaPercentL1);
+	console.log("Percentual L2: " + calculaPercentL2);
+	console.log("Percentual L3: " + calculaPercentL3);
+	console.log("Percentual L4: " + calculaPercentL4);
+
 
 // Configura gaugues
 	var config10c = liquidFillGaugeDefaultSettings();
@@ -166,9 +157,12 @@ queue()
 	 var gauge10c = loadLiquidFillGauge("fillgauge10c", lastVolC10, config10c);  //cisterna bloco 10
 	 //var fillbomba = document.getElementById('bomba').innerHTML = bomba;
 
+	 var pieChartBloco10 = loadPieChart("chartbl10", calculaPercentL0, calculaPercentL1, calculaPercentL2, calculaPercentL3, calculaPercentL4);
 
-	 var chart = new CanvasJS.Chart("chartbl10",
-	{
+	 function loadPieChart(elemenetId, value0, value1, value2, value3, valeu4) {
+
+	 	 var chart = new CanvasJS.Chart(elemenetId, {
+			
 		backgroundColor: "#D5F0FD",
 		title:{
 			text: "Historico dos últimos 10 dias"
@@ -191,16 +185,17 @@ queue()
 			showInLegend: false,
 			toolTipContent:"{legendText} {y}%",
 			dataPoints: [
-				{  y: calculaPercentL0, legendText:"Vazio", label: "Vazio", color: "#E53A0F"},
-				{  y: calculaPercentL1, legendText:"Baixo", label: "Baixo", color: "#FFA500" },
-				{  y: calculaPercentL2, legendText:"Medio", label: "Medio", color: "#178BCA"},
-				{  y: calculaPercentL3, legendText:"Alto" , label: "Alto", color:"#008100"},
-				{  y: calculaPercentL4, legendText:"Transbordo" , label: "Transbordo", color:"#800080"}
+				{  y: value0, legendText:"Vazio", label: "Vazio", color: "#E53A0F"},
+				{  y: value1, legendText:"Baixo", label: "Baixo", color: "#FFA500" },
+				{  y: value2, legendText:"Medio", label: "Medio", color: "#178BCA"},
+				{  y: value3, legendText:"Alto" , label: "Alto", color:"#008100"},
+				{  y: valeu4, legendText:"Transbordo" , label: "Transbordo", color:"#800080"}
 			]
 		}
 		]
 	});
 	chart.render();
+	}
 
 };
 });
